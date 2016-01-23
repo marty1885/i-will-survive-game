91901cc225b3b39203b26459a844c8560c579163
marty185 <marty188586@gamil.com>
2016-01-23 12:56:56 +0800

    first commit

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- Configuration
+function love.conf(t)
+	t.title = "Test Game" -- The title of the window the game is in (string)
+	t.version = "0.9.1"         -- The LÖVE version this game was made for (string)
+	t.window.width = 1200        -- we want our game to be long and thin.
+	t.window.height = 720
+
+	-- For Windows debugging
+	t.console = true
+end
---
+++ main.lua
@@ -0,0 +1,13 @@
+debug = true
+
+function love.load(arg)
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw(dt)
+	love.graphics.print("Hello World!", 400, 300)
+end
